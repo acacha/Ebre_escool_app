b1a5f3cf448f422bd5981155c055abfa489c60a2
Sergi Tur <sergiturbadenas@gmail.com>
2015-01-27 17:35:43 +0100

    sync

---
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/EbreEscoolAccount.java
@@ -0,0 +1,31 @@
+package org.acacha.ebre_escool.ebre_escool_app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Udini
+ * Date: 20/03/13
+ * Time: 18:11
+ */
+public class EbreEscoolAccount {
+
+    /**
+     * Account type id
+     */
+    public static final String ACCOUNT_TYPE = "org.acacha.ebre_escool.ebre_escool_app";
+
+    /**
+     * Account name
+     */
+    public static final String ACCOUNT_NAME = "EbreEscool";
+
+    /**
+     * Auth token types
+     */
+    public static final String AUTHTOKEN_TYPE_READ_ONLY = "Read only";
+    public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to an EbreEscool account";
+
+    public static final String AUTHTOKEN_TYPE_FULL_ACCESS = "Full access";
+    public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to an EbreEscool account";
+
+    //public static final ServerAuthenticate sServerAuthenticate = new ParseComServerAuthenticate();
+}
