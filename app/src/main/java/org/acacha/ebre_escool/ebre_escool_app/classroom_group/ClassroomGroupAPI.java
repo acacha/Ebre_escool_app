d445cc070de56f6a84f78aeda912824be173e3a7
Rafael <rafi.93.zgz@gmail.com>
2015-02-06 16:17:50 +0100

    reparar

---
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/classroom_group/ClassroomGroupAPI.java
@@ -0,0 +1,14 @@
+package org.acacha.ebre_escool.ebre_escool_app.apis;
+
+/**
+ * Created by sergi on 30/01/15.
+ */
+public class ClassroomGroupAPI {
+    // URL OF PUBLIC API EBRE-ESCOOL
+    public final static String EBRE_ESCOOL_PUBLIC_API_URL =
+            "http://acacha.org/acacha_manager/index.php/rafaelRicote/api/classroomgroup";
+
+    //URL of image no available
+    public final static String EBRE_ESCOOL_PUBLIC_IMAGE_NOT_AVAILABLE =
+            "http://acacha.org/acacha_manager/uploads/school_logos/no_picture.gif";
+}
