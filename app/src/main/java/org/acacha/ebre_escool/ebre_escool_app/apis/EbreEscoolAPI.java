88f0f6e7c8896bbdad657fbdcfaea741647e7fc8
Paolo Davila <pdavila@iesebre.com>
2015-02-09 19:08:36 +0100

    TODO

--- app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/apis/EbreEscoolAPI.java
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/apis/EbreEscoolAPI.java
@@ -5,13 +5,6 @@ ackage org.acacha.ebre_escool.ebre_escool_app.apis;
 public class EbreEscoolAPI {
     // URL OF PUBLIC API EBRE-ESCOOL
     public final static String EBRE_ESCOOL_PUBLIC_API_URL =
-<<<<<<< HEAD
-            "http://192.168.160.106/tauriel/api";
-=======
             "http://acacha.org/acacha_manager/index.php/ebre_escool/api";
 
-    //URL of image no available
-    public final static String EBRE_ESCOOL_PUBLIC_IMAGE_NOT_AVAILABLE =
-            "http://acacha.org/acacha_manager/uploads/school_logos/no_picture.gif";
->>>>>>> 11152884b7ce5c745cc60b4e883d9c26d8aaae97
 }
---
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/employees/EmployeesAPI.java
@@ -0,0 +1,10 @@
+package org.acacha.ebre_escool.ebre_escool_app.apis;
+
+/**
+ * Created by sergi on 30/01/15.
+ */
+public class EmployeesAPI {
+    // URL OF PUBLIC API EBRE-ESCOOL
+    public final static String EBRE_ESCOOL_PUBLIC_API_URL =
+            "http://192.168.160.106/index.php/tauriel/api/employees";
+}
