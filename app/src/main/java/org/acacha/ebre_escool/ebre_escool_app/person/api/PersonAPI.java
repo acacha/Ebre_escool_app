82ed29a505518b802adbe0254c1af6988d64b487
nicolaeturcan <nicolaeturcan@iesebre.com>
2015-02-11 20:22:46 +0100

    New Update

---
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/person/api/PersonAPI.java
@@ -0,0 +1,14 @@
+package org.acacha.ebre_escool.ebre_escool_app.person.api;
+
+/**
+ * Created by nicolae on 30/01/15.
+ */
+public class PersonAPI {
+    // URL OF PUBLIC API EBRE-ESCOOL
+    public final static String EBRE_ESCOOL_PUBLIC_API_URL =
+            "http://185.13.76.85:8770/ebre-escool/index.php/nicolaeturcan/api/persons";
+
+    //URL of image no available
+    public final static String EBRE_ESCOOL_PUBLIC_IMAGE_NOT_AVAILABLE =
+            "/usr/share/ebre-escool/uploads/person_photos";
+}
