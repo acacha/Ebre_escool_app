c561985ae3950dcaf6cdee969bf4608445676724
Alexandre <alexmart4@gmail.com>
2015-02-13 16:02:39 +0100

    Canvis en el fragment

--- app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/managmentsandbox/study_submodules/api/StudySubmoduleApi.java
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/managmentsandbox/study_submodules/api/StudySubmoduleApi.java
@@ -7,12 +7,12 @@ public class StudySubmoduleApi {
 
     // URL OF PUBLIC API EBRE-ESCOOL
     /* public final static String EBRE_ESCOOL_PUBLIC_API_URL =
-    "http://185.13.76.85:8765/ebre-escool/index.php/alexMartinez/api/studysubmoduels/"*/;
+    "http://185.13.76.85:8765/ebre-escool/index.php/alexMartinez/api/studysubmodules/"*/;
     ////////////////////////////////////////////////////////////////////////////////////
 
     // URL OF PUBLIC API EBRE-ESCOOL
     public final static String EBRE_ESCOOL_PUBLIC_API_URL =
-            "http://localhost/ebre-escool/index.php/alexMartinez/api/studysubmoduels/";
+            "http://localhost/ebre-escool/index.php/alexMartinez/api/studysubmodules/";
 
     //URL of image no available
     public final static String EBRE_ESCOOL_PUBLIC_IMAGE_NOT_AVAILABLE =
