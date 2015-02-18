5d2e42ff76e8f95b7c29b92fb54d1af79ac3e309
Dorian <dorian.un22@gmail.com>
2015-02-18 20:48:24 +0100

    API

---
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/managmentsandbox/users/api/UsersApiService.java
@@ -0,0 +1,24 @@
+package org.acacha.ebre_escool.ebre_escool_app.managmentsandbox.users.api;
+
+import org.acacha.ebre_escool.ebre_escool_app.managmentsandbox.study_submodules.pojos.StudySubmodules;
+
+import java.util.List;
+import java.util.Map;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Created by dorian on 11/02/15.
+ */
+public interface UsersApiService {
+//    @GET("/users")
+//    void personsAsList(Callback<List<users>> cb);
+//
+//    @GET("/users")
+//    void persons(Callback<Map<String, users>> cb);
+//
+//    @GET("/users/{id}")
+//    void person(Callback<users> cb, @Path("id") String id);
+}
---
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/managmentsandbox/users/pojos/Users.java
@@ -0,0 +1,7 @@
+package org.acacha.ebre_escool.ebre_escool_app.managmentsandbox.users.pojos;
+
+/**
+ * Created by dorian on 11/02/15.
+ */
+public class Users {
+}
