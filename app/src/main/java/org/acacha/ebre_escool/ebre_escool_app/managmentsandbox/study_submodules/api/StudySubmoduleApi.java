7bfadc39ef1b428ffc07d2edbb4dc14ca37536f2
Alexandre <alexmart4@gmail.com>
2015-02-23 19:57:48 +0100

    Canvis significatius pero encara no estan acabats

--- app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/managmentsandbox/study_submodules/api/StudySubmoduleApi.java
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/managmentsandbox/study_submodules/api/StudySubmoduleApi.java
@@ -12,9 +12,21 @@ public class StudySubmoduleApi {
 
     // URL OF PUBLIC API EBRE-ESCOOL
     public final static String EBRE_ESCOOL_PUBLIC_API_URL =
-            "http://localhost/ebre-escool/index.php/alexMartinez/api/studysubmodules/";
+            "http://192.168.202.174/ebre-escool/index.php/alexMartinez/api/studysubmodules/";
+            //"http://localhost/ebre-escool/index.php/alexMartinez/api/studysubmodules/";
 
     //URL of image no available
-    public final static String EBRE_ESCOOL_PUBLIC_IMAGE_NOT_AVAILABLE =
+    /*public final static String EBRE_ESCOOL_PUBLIC_IMAGE_NOT_AVAILABLE =
             "http://acacha.org/acacha_manager/uploads/school_logos/no_picture.gif";
+    */
+
+    public static final String API_KEY ="856486bb57a9d48eec1b475e8e57998c";
+
+    //Image url
+    public static final String IMAGE = "http://img.unionfansub.com/albums/userpics/14184/Logo_para_Union_Fansub_28shrink29.jpg";
+    public static final String ACTION ="action";
+    public static final String DETAIL ="detail";
+    public static final String EDIT ="edit";
+    public static final String PUT ="put";
 }
+
